3ffc4b24b6745cc55a8bc2459423bf7c7406769a
SeeDashMusic <85347510+SHS666638@users.noreply.github.com>
2021-08-15 21:38:16 +0700

    update run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+echo "Installing Bash..."
+sleep 2s
+echo "C:/Bash/rythmserver/ Running"
+sleep 2s
+echo "Select Running:"
+select Bash
+echo "Bash V2"
+echo "Select Shell to Learn:"
+echo "Hi.sh"
+read ji
+echo $ji Learn:
+    echo "echo Hello!"
